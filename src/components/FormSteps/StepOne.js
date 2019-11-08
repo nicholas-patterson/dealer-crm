c3c1adc0b7a1ed95057c00f4e3527d31c9921f00
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-11-08 15:06:43 -0500

    :recycle: adds placeholders instead of labels
    adds placeholders instead of labels

--- src/components/FormSteps/StepOne.js
+++ src/components/FormSteps/StepOne.js
@@ -6,35 +6,52 @@ const StepOne = props => {
   };
 
   return (
-    <form>
+    <form className="stepOne">
       <label htmlFor="email">
+        <i class="icon fas fa-envelope"></i>
         <input
           type="email"
           id="email"
           name="email"
           value={props.value.email}
           onChange={props.handleChange}
+          placeholder="Email..."
         />
       </label>
       <label htmlFor="username">
+        <i className="icon fas fa-user"></i>
         <input
           type="text"
           id="username"
           name="username"
           value={props.value.username}
           onChange={props.handleChange}
+          placeholder="Username..."
         />
       </label>
       <label htmlFor="password">
+        <i className="icon fas fa-lock"></i>
         <input
           type="password"
           id="password"
           name="password"
           value={props.value.password}
           onChange={props.handleChange}
+          placeholder="Password..."
         />
       </label>
-      <button onClick={saveAndContinue}>Continue</button>
+
+      {props.value.email.length === 0 ||
+      props.value.username.length === 0 ||
+      props.value.password.length === 0 ? (
+        <button disabled className="disabledBtn button">
+          Continue
+        </button>
+      ) : (
+        <button className="continueBtn button" onClick={saveAndContinue}>
+          Continue
+        </button>
+      )}
     </form>
   );
 };

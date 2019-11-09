872fb1bb774d8d4a508364a11f736d794c679176
Elvis Knapman <elvis@elvisknapman.com>
2019-11-09 18:30:25 -0500

    :sparkles: Created Site Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SiteLogo from "../../images/AUTOACUITYLOGO-sm.png";
+
+const Logo = () => {
+  return (
+    <div className="form-logo">
+      <img src={SiteLogo} alt="Auto Acuity logo" />
+    </div>
+  );
+};
+
+export default Logo;

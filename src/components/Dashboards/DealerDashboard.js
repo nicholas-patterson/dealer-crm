9b48a1625d02711c01e8f9eb98497f7c4e895b2f
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-11-13 00:37:21 -0500

    :sparkles: adds logo and sidenav component and adds Name to the end of class
    adds logo and sidenav component and adds Name to the end of class

--- src/components/Dashboards/DealerDashboard.js
+++ src/components/Dashboards/DealerDashboard.js
@@ -1,162 +1,166 @@
 import React from "react";
+import SideNav from "../SideNav";
+import Logo from "../Logo/Logo";
 
 const DealerDashboard = props => {
   return (
-    <div class="dealer-container">
-      <div class="nav-container">
-        <div class="nav-bar">Sidebar</div>
-        {/* SIDEBAR START */}
+    <div className="dealer-container">
+      <div className="nav-container">
+        <div className="nav-bar">
+          <Logo />
+          <SideNav />
+        </div>
       </div>
 
-      <div class="main-content-wrapper">
-        <div class="main-content-container">
-          <div class="header-dealer">
-            <div class="header-dealer__name">Dealership: Ford</div>
-            <div class="header-dealer__notifications">Notifications</div>
+      <div className="main-content-wrapper">
+        <div className="main-content-container">
+          <div className="header-dealer">
+            <div className="header-dealer__name">Dealership: Ford</div>
+            <div className="header-dealer__notifications">Notifications</div>
           </div>
 
           {/* ACTION AND RECENT ACTIVITY BOXES */}
 
-          <div class="console-recent-activity-container">
+          <div className="console-recent-activity-container">
             {/* START CONSOLE */}
-            <div class="console">
-              <h2 class="console__heading">what would you like to do?</h2>
+            <div className="console">
+              <h2 className="console__heading">what would you like to do?</h2>
 
-              <div class="console__controls">
-                <div class="console__control">
+              <div className="console__controls">
+                <div className="console__control">
                   <p>Add New Lead</p>
                 </div>
 
-                <div class="console__control">
+                <div className="console__control">
                   <p>Add New Salesperson</p>
                 </div>
 
-                <div class="console__control">
+                <div className="console__control">
                   <p>Add New Manager</p>
                 </div>
               </div>
             </div>
 
             {/* START RECENT ACTIVITY */}
-            <div class="recent-activity">
-              <h2 class="recent-activity__heading">
+            <div className="recent-activity">
+              <h2 className="recent-activity__heading">
                 recent activity{" "}
                 <span
-                  class="iconify recent-activity__help-icon"
+                  className="iconify recent-activity__help-icon"
                   data-icon="mdi:map-marker-question-outline"
                   data-inline="false"
                 ></span>
               </h2>
-              <div class="recent-activity__items">
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+              <div className="recent-activity__items">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:account-badge-alert-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new lead
                   </div>
-                  <div class="recent-activity__item__time">Just now</div>
+                  <div className="recent-activity__item__time">Just now</div>
                 </div>
 
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:alert-rhombus-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new salesperson
                   </div>
-                  <div class="recent-activity__item__time">1 hour ago</div>
+                  <div className="recent-activity__item__time">1 hour ago</div>
                 </div>
 
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:alert-rhombus-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new manager
                   </div>
-                  <div class="recent-activity__item__time">3 hours ago</div>
+                  <div className="recent-activity__item__time">3 hours ago</div>
                 </div>
 
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:alert-circle-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new vehicle
                   </div>
-                  <div class="recent-activity__item__time">4 hours ago</div>
+                  <div className="recent-activity__item__time">4 hours ago</div>
                 </div>
 
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:alert-circle-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new vehicle
                   </div>
-                  <div class="recent-activity__item__time">4 hours ago</div>
+                  <div className="recent-activity__item__time">4 hours ago</div>
                 </div>
 
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:account-badge-alert-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new lead
                   </div>
-                  <div class="recent-activity__item__time">6 hours ago</div>
+                  <div className="recent-activity__item__time">6 hours ago</div>
                 </div>
 
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:account-badge-alert-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new lead
                   </div>
-                  <div class="recent-activity__item__time">6 hours ago</div>
+                  <div className="recent-activity__item__time">6 hours ago</div>
                 </div>
-                <div class="recent-activity__item">
-                  <div class="recent-activity__item__box recent-activity__item__icon">
+                <div className="recent-activity__item">
+                  <div className="recent-activity__item__box recent-activity__item__icon">
                     <span
-                      class="iconify"
+                      className="iconify"
                       data-icon="mdi:account-badge-alert-outline"
                       data-inline="false"
                     ></span>
                   </div>
-                  <div class="recent-activity__item__box recent-activity__item__text">
+                  <div className="recent-activity__item__box recent-activity__item__text">
                     You added a new lead
                   </div>
-                  <div class="recent-activity__item__time">6 hours ago</div>
+                  <div className="recent-activity__item__time">6 hours ago</div>
                 </div>
               </div>
               {/* END RECENT ACTIVITY */}

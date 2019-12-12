b18af1a3013422664800c4254edada5304f33766
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-12 17:04:30 -0500

    :sparkles: adds code for react portal
    adds code for react portal

---
+++ src/components/Portal.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const portalRoot = document.getElementById("portal");
+
+class Portal extends Component {
+  constructor() {
+    super();
+    this.el = document.createElement("div");
+  }
+
+  componentDidMount = () => {
+    portalRoot.appendChild(this.el);
+  };
+
+  componentWillUnMount = () => {
+    portalRoot.removeChild(this.el);
+  };
+
+  render() {
+    return ReactDOM.createPortal(
+      <div
+        style={{
+          position: "absolute",
+          top: "0",
+          bottom: "0",
+          left: "0",
+          right: "0",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0,0,0,0.6)"
+        }}
+      >
+        {this.props.children}
+      </div>,
+      this.el
+    );
+  }
+}
+
+export default Portal;

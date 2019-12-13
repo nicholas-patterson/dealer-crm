800c698ab41b3b0b0d42838d6a4dc3b0e1d923c1
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-12 21:13:20 -0500

    :sparkles: adds click events for sidenav
    adds click events for sidenav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -6,32 +6,58 @@ import carGarage from "@iconify/icons-si-glyph/car-garage";
 import roundHelpOutline from "@iconify/icons-ic/round-help-outline";
 import sharLogOut from "@iconify/icons-ic/sharp-log-out";
 import settingsIcon from "@iconify/icons-feather/settings";
+import { connect } from "react-redux";
+import { getDashboardLink, getLeadsLink } from "../actions";
 
-const SideNav = () => {
+const SideNav = props => {
+  const handleDashboardClick = () => {
+    props.getDashboardLink("dashboard");
+  };
+
+  const handleLeadsClick = () => {
+    props.getDashboardLink("leads");
+  };
+
+  const handleInventoryClick = () => {
+    props.getDashboardLink("inventory");
+  };
+
+  const handleAccountClick = () => {
+    props.getDashboardLink("account");
+  };
+
+  const handleHelpClick = () => {
+    props.getDashboardLink("help");
+  };
+
+  const handleLogoutClick = () => {
+    props.getDashboardLink("logout");
+  };
+
   return (
     <>
       <div className="sidebar__links">
-        <a href="#" className="sidebar__link">
+        <a href="#" onClick={handleDashboardClick} className="sidebar__link">
           <Icon className="iconify" icon={outlineBubbleChart} />
           dashboard
         </a>
-        <a href="#" className="sidebar__link">
+        <a href="#" className="sidebar__link" onClick={handleLeadsClick}>
           <Icon className="iconify" icon={usersIcon} />
           leads
         </a>
-        <a href="#" className="sidebar__link">
+        <a href="#" className="sidebar__link" onClick={handleInventoryClick}>
           <Icon className="iconify" icon={carGarage} />
           inventory
         </a>
-        <a href="#" className="sidebar__link">
+        <a href="#" className="sidebar__link" onClick={handleAccountClick}>
           <Icon className="iconify" icon={settingsIcon} />
           Account
         </a>
-        <a href="#" className="sidebar__link">
+        <a href="#" className="sidebar__link" onClick={handleHelpClick}>
           <Icon className="iconify" icon={roundHelpOutline} />
           help
         </a>
-        <a href="#" className="sidebar__link">
+        <a href="#" className="sidebar__link" onClick={handleLogoutClick}>
           <Icon className="iconify" icon={sharLogOut} />
           Log out
         </a>
@@ -40,4 +66,12 @@ const SideNav = () => {
   );
 };
 
-export default SideNav;
+const mapStateToProps = state => {
+  return {
+    link: state.dealerNavigationReducer.link
+  };
+};
+
+export default connect(mapStateToProps, { getDashboardLink, getLeadsLink })(
+  SideNav
+);

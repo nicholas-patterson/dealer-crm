e2bd6c1603fa1f0661bd1adba09fd2488cbe0232
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-12 21:12:25 -0500

    :sparkles: adds actions for navigation links
    adds actions for navigation links

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+export const getDashboardLink = link => {
+  return {
+    type: "FETCH_DASHBOARD_LINK_SUCCESS",
+    payload: link
+  };
+};
+
+export const getLeadsLink = link => {
+  return {
+    type: "FETCH_DASHBOARD_LINK_SUCCESS",
+    payload: link
+  };
+};
+
+export const getInventoryLink = link => {
+  return {
+    type: "FETCH_DASHBOARD_LINK_SUCCESS",
+    payload: link
+  };
+};

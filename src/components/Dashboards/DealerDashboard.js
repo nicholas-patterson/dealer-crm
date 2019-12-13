c52abc3ce7cc7f4c7d98f1425a038f49ab9789a5
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-12 21:14:54 -0500

    :sparkles: adds switch for each page in the navigation
    adds switch for each page in the navigation

--- src/components/Dashboards/DealerDashboard.js
+++ src/components/Dashboards/DealerDashboard.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import SideNav from "../SideNav";
 import Logo from "../Logo/Logo";
 import Portal from "../Portal";
+import DealerDashboardMain from "../SwitchItems/DealerDashboardMain";
+import DealerLeadsMain from "../SwitchItems/DealerLeadsMain";
+import DealerInventoryMain from "../SwitchItems/DealerInventoryMain";
+import DealerAccountMain from "../SwitchItems/DealerAccountMain";
+import DealerHelpMain from "../SwitchItems/DealerHelpMain";
+import { connect } from "react-redux";
 
 const DealerDashboard = props => {
   const [isShowing, setIsShowing] = useState("");
@@ -48,12 +54,37 @@ const DealerDashboard = props => {
     setManagerInfo({ ...managerInfo, [e.target.name]: e.target.value });
   };
 
+  const pageSwitch = () => {
+    switch (props.dash) {
+      case "dashboard":
+        return (
+          <DealerDashboardMain
+            leadsClick={leadClick}
+            salesClick={salesClick}
+            managerClick={managerClick}
+          />
+        );
+      case "leads":
+        return <DealerLeadsMain />;
+
+      case "inventory":
+        return <DealerInventoryMain />;
+      case "account":
+        return <DealerAccountMain />;
+      case "help":
+        return <DealerHelpMain />;
+      default:
+        return null;
+    }
+  };
+
   const modalSwitch = () => {
     switch (isShowing) {
       case "leads":
         return (
           <Portal>
             <form className="leads-form" onSubmit={handleSubmit}>
+              <h3 className="leads-form_title">Create A Lead..</h3>
               <label>
                 <input
                   type="text"
@@ -124,7 +155,7 @@ const DealerDashboard = props => {
                   placeholder="State"
                 />
               </label>
-              <button className="continueBtn">Submit</button>
+              <button className="submitBtn">Submit</button>
             </form>
           </Portal>
         );
@@ -269,175 +300,7 @@ const DealerDashboard = props => {
         </div>
 
         <div className="main-content-wrapper">
-          <div className="main-content-container">
-            <div className="header-dealer">
-              <div className="header-dealer__name">Dealership: Ford</div>
-              <div className="header-dealer__notifications">Notifications</div>
-            </div>
-            {/* ACTION AND RECENT ACTIVITY BOXES */}
-            t.
-            <div className="console-recent-activity-container">
-              {/* START CONSOLE */}
-              <div className="console">
-                <h2 className="console__heading">what would you like to do?</h2>
-
-                <div className="console__controls">
-                  <div className="console__control" onClick={leadClick}>
-                    <p>Add New Lead</p>
-                  </div>
-
-                  <div className="console__control" onClick={salesClick}>
-                    <p>Add New Salesperson</p>
-                  </div>
-
-                  <div className="console__control" onClick={managerClick}>
-                    <p>Add New Manager</p>
-                  </div>
-                </div>
-              </div>
-
-              {/* START RECENT ACTIVITY */}
-              <div className="recent-activity">
-                <h2 className="recent-activity__heading">
-                  recent activity{" "}
-                  <span
-                    className="iconify recent-activity__help-icon"
-                    data-icon="mdi:map-marker-question-outline"
-                    data-inline="false"
-                  ></span>
-                </h2>
-                <div className="recent-activity__items">
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:account-badge-alert-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new lead
-                    </div>
-                    <div className="recent-activity__item__time">Just now</div>
-                  </div>
-
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:alert-rhombus-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new salesperson
-                    </div>
-                    <div className="recent-activity__item__time">
-                      1 hour ago
-                    </div>
-                  </div>
-
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:alert-rhombus-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new manager
-                    </div>
-                    <div className="recent-activity__item__time">
-                      3 hours ago
-                    </div>
-                  </div>
-
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:alert-circle-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new vehicle
-                    </div>
-                    <div className="recent-activity__item__time">
-                      4 hours ago
-                    </div>
-                  </div>
-
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:alert-circle-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new vehicle
-                    </div>
-                    <div className="recent-activity__item__time">
-                      4 hours ago
-                    </div>
-                  </div>
-
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:account-badge-alert-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new lead
-                    </div>
-                    <div className="recent-activity__item__time">
-                      6 hours ago
-                    </div>
-                  </div>
-
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:account-badge-alert-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new lead
-                    </div>
-                    <div className="recent-activity__item__time">
-                      6 hours ago
-                    </div>
-                  </div>
-                  <div className="recent-activity__item">
-                    <div className="recent-activity__item__box recent-activity__item__icon">
-                      <span
-                        className="iconify"
-                        data-icon="mdi:account-badge-alert-outline"
-                        data-inline="false"
-                      ></span>
-                    </div>
-                    <div className="recent-activity__item__box recent-activity__item__text">
-                      You added a new lead
-                    </div>
-                    <div className="recent-activity__item__time">
-                      6 hours ago
-                    </div>
-                  </div>
-                </div>
-                {/* END RECENT ACTIVITY */}
-
-                {/* END ACTION AND RECENT ACTIVITY BOXES */}
-              </div>
-            </div>
-          </div>
+          <div className="main-content-container">{pageSwitch()}</div>
         </div>
       </div>
       {modalSwitch()}
@@ -445,4 +308,10 @@ const DealerDashboard = props => {
   );
 };
 
-export default DealerDashboard;
+const mapStateToProps = state => {
+  return {
+    dash: state.dealerNavigationReducer.link
+  };
+};
+
+export default connect(mapStateToProps, {})(DealerDashboard);

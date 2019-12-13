6608cf668565ba47379461b7e9e4b728702e64b7
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-12 21:11:37 -0500

    :sparkles: adds redux to project
    adds redux to project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/main.scss";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

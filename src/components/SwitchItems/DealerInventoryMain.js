096af87606ecf934d5f30ee685284cc4943e983a
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-12 21:18:02 -0500

    :sparkles: adds inventory page for inventory link in nav
    adds inventory page for inventory link in nav

---
+++ src/components/SwitchItems/DealerInventoryMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DealerInventoryMain = () => {
+  return (
+    <>
+      <h1>This is the Inventory Page</h1>
+    </>
+  );
+};
+
+export default DealerInventoryMain;

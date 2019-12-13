59fb9e85435fa0c6baa96eb97f5eb16c53dfac12
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-12-13 17:24:26 -0500

    :sparkles: adds nested routes for deeper routes
    adds nested routes for deeper routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import WelcomePage from "./components/WelcomePage";
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LogInForm";
 import DealerDashBoard from "./components/Dashboards/DealerDashboard";
+import DealerDashboardMain from "./components/SwitchItems/DealerDashboardMain";
+import DealerLeadsMain from "./components/SwitchItems/DealerLeadsMain";
+import DealerInventoryMain from "./components/SwitchItems/DealerInventoryMain";
+import DealerHelpMain from "./components/SwitchItems/DealerHelpMain";
+import DealerAccountMain from "./components/SwitchItems/DealerAccountMain";
+
 import { Router } from "@reach/router";
 
 const App = () => {
@@ -12,7 +18,13 @@ const App = () => {
         <WelcomePage default="/" />
         <SignUpForm path="/signup" />
         <LoginForm path="/login" />
-        <DealerDashBoard path="/dealer" />
+        <DealerDashBoard path="/dealer">
+          <DealerDashboardMain path="/dash" />
+          <DealerLeadsMain path="/leads" />
+          <DealerInventoryMain path="/inventory" />
+          <DealerHelpMain path="/help" />
+          <DealerAccountMain path="/account" />
+        </DealerDashBoard>
       </Router>
     </div>
   );

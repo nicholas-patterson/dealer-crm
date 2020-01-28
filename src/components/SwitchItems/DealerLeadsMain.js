538e35d6f465a573f1b710fbe0805b6be3145d03
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-01-27 20:12:37 -0500

    :art: removed all divs and added a table for leads list
    removed all divs and added a table for leads list

--- src/components/SwitchItems/DealerLeadsMain.js
+++ src/components/SwitchItems/DealerLeadsMain.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Icon } from "@iconify/react";
-import newButton from "@iconify/icons-emojione-monotone/new-button";
 import editOutline from "@iconify/icons-ant-design/edit-outline";
 import deleteOutline from "@iconify/icons-ant-design/delete-outline";
 
@@ -18,80 +17,39 @@ const DealerLeadsMain = () => {
 
       <div className="leads">
         <div className="leads__heading">Recent Leads</div>
-        <div className="categories">
-          <ul className="categories__category">
-            <li>First Name</li>
-            <li>Address</li>
-            <li>Type</li>
-            <li>Edit</li>
-            <li>Delete</li>
-          </ul>
-        </div>
 
-        {/* Start Leads List */}
-
-        <div className="recent-leads__items">
-          <div className="recent-leads__item">
-            <div className="recent-leads__item__box recent-leads__item__icon">
-              <Icon icon={newButton} />
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__name">
-              Nicholas Pattterson
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__address">
-              1500 Redis Lane Javascript, Js 18999
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__type">
-              Referral
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__edit">
+        <table>
+          <tr>
+            <th>Full Name</th>
+            <th>Address</th>
+            <th>Type</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+          <tr class="spaceunder"></tr>
+          <tr>
+            <td>Nicholas Patterson</td>
+            <td>1500 Redis Lane, Javascript, Js 18999</td>
+            <td>Referral</td>
+            <td className="edit-style">
               <Icon icon={editOutline} />
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__delete">
+            </td>
+            <td className="delete-style">
               <Icon icon={deleteOutline} />
-            </div>
-          </div>
-          <div className="recent-leads__item">
-            <div className="recent-leads__item__box recent-leads__item__icon">
-              <Icon icon={newButton} />
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__name">
-              Nicholas Pattterson
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__address">
-              1500 Redis Lane Javascript, Js 18999
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__type">
-              Referral
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__edit">
+            </td>
+          </tr>
+          <tr>
+            <td>Elvis Knapman</td>
+            <td>1100 Json WebToken Pl, JWT, 18787</td>
+            <td>Walk in</td>
+            <td className="edit-style">
               <Icon icon={editOutline} />
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__delete">
+            </td>
+            <td className="delete-style">
               <Icon icon={deleteOutline} />
-            </div>
-          </div>
-          <div className="recent-leads__item">
-            <div className="recent-leads__item__box recent-leads__item__icon">
-              <Icon icon={newButton} />
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__name">
-              Nicholas Pattterson
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__address">
-              1500 Redis Lane Javascript, Js 18999
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__type">
-              Referral
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__edit">
-              <Icon icon={editOutline} />
-            </div>
-            <div className="recent-leads__item__box recent-leads__item__delete">
-              <Icon icon={deleteOutline} />
-            </div>
-          </div>
-        </div>
+            </td>
+          </tr>
+        </table>
       </div>
     </>
   );

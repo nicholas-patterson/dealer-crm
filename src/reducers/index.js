b046ace2f91597ebe3320940e9e5deae03deade4
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-05 18:41:47 -0500

    :sparkles: adds all imports to combineReducers
    adds all imports to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,20 @@
 import { combineReducers } from "redux";
 import { dealerNavigationReducer } from "./dealerNavigationReducer";
 import { userReducer } from "./userReducer";
+import { addLeadReducer } from "./addLeadReducer";
+import { addSalespersonReducer } from "./addSalespersonReducer";
+import { deleteSalespersonReducer } from "./deleteSalespersonReducer";
+import { editLeadReducer } from "./editLeadReducer";
+import { loginReducer } from "./loginReducer";
+import { registerReducer } from "./registerReducer";
 
 export default combineReducers({
   dealerNavigationReducer,
-  userReducer
+  userReducer,
+  addLeadReducer,
+  addSalespersonReducer,
+  deleteSalespersonReducer,
+  editLeadReducer,
+  loginReducer,
+  registerReducer
 });

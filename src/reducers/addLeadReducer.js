c5413eff76276ef45d870ee0a654810add6289b3
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-05 18:41:23 -0500

    :sparkles: adds addLeadReducer
    adds addLeadReducer

---
+++ src/reducers/addLeadReducer.js
@@ -0,0 +1,29 @@
+const initalState = {
+  lead: [],
+  isLoading: false,
+  error: ""
+};
+
+export const addLeadReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "ADD_LEAD_START":
+      return {
+        ...state,
+        isLoading: true
+      };
+    case "ADD_LEAD_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        lead: action.payload
+      };
+    case "ADD_LEAD_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};

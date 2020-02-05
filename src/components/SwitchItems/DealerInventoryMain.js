f3c66a4a720d7ea9c62cfa45f2efabdcd20cbcbf
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-05 15:56:45 -0500

    :sparkles: adds modal to add inventory only with file upload
    adds modal to add inventory only with file upload

--- src/components/SwitchItems/DealerInventoryMain.js
+++ src/components/SwitchItems/DealerInventoryMain.js
@@ -1,68 +1,296 @@
 import React, { useState } from "react";
-import {Link} from "@reach/router";
+import { Link } from "@reach/router";
 import Portal from "../Portal";
+import {connect} from "react-redux";
 
-const DealerInventoryMain = (props) => {
-  
+const DealerInventoryMain = props => {
+  console.log("PROPS IN INV", props)
+  // new inventory state search
   const [newInventory, setNewInventory] = useState({
     new_year: "",
     new_make: "",
     new_model: ""
   });
+  // used inventory state search
   const [usedInventory, setUsedInventory] = useState({
     used_year: "",
     used_make: "",
     used_model: ""
   });
 
+  // used inventory add state
+  const [usedInventoryAdd, setUsedInventoryAdd] = useState({
+    car_picture: "",
+    year: "",
+    make: "",
+    model: "",
+    price: "",
+    miles: ""
+  });
+
+   // new inventory add state
+   const [newInventoryAdd, setNewInventoryAdd] = useState({
+    car_picture: "",
+    year: "",
+    make: "",
+    model: "",
+    price: "",
+    miles: ""
+  });
+
+  console.log("FILE",usedInventoryAdd)
+
+  // new inventory search
   const handleNewInventoryChange = e => {
     setNewInventory({ ...newInventory, [e.target.name]: e.target.value });
   };
 
+  // used inventory search
   const handleUsedInventoryChange = e => {
     setUsedInventory({ ...usedInventory, [e.target.name]: e.target.value });
   };
 
+  // on click for new inventory
   const handleNewInventoryAdd = e => {
     setShowNewInventoryModal(true);
-  }
+    setNewInventoryAdd({...newInventoryAdd, [e.target.name]:e.target.value})
 
+  };
+  // on click for used inventory
   const handleUsedInventoryAdd = e => {
     setShowUsedInventoryModal(true);
-  }
+    setUsedInventoryAdd({
+      ...usedInventoryAdd,
+      [e.target.name]: e.target.value
+    });
+  };
 
+  
+  // Set inventory state to false
   const [showUsedInventoryModal, setShowUsedInventoryModal] = useState(false);
   const [showNewInventoryModal, setShowNewInventoryModal] = useState(false);
 
+  // handle used inventory close button
+  const handleUsedInventoryCloseButton = () => {
+    setShowUsedInventoryModal(false);
+  }
+
+  const handleNewInventoryCloseButton = () => {
+    setShowNewInventoryModal(false);
+  }
 
   return (
     <>
-    {showUsedInventoryModal ? 
-     <Portal>
-        <form>
-          <input type="file" name="car_picture"/>
-          <input type="text" name="year"/>
-          <input type="text" name="make"/>
-          <input type="text" name="model"/>
-          <input type="text" name="price"/>
-          <input type="text" name="miles"/>
+      {showUsedInventoryModal ? (
+        <Portal>
+          <form className="usedInventoryAddForm">
+            <div className="usedInv-field" id="picture_file">
+              <input
+                type="file"
+                id="car_picture"
+                name="car_picture"
+                className="usedInv-group"
+                value={usedInventoryAdd.car_picture}
+                onChange={handleUsedInventoryAdd}
+              />
+              <label for="car_picture">Upload file</label>
+            </div>
 
-          <textarea type="car info" name="info"></textarea>
-        </form>
-     </Portal>: null}
-     {showNewInventoryModal ? 
-     <Portal>
-         <form>
-          <input type="file" name="car_picture"/>
-          <input type="text" name="year"/>
-          <input type="text" name="make"/>
-          <input type="text" name="model"/>
-          <input type="text" name="price"/>
-          <input type="text" name="miles"/>
+            <div className="usedInv-field">
+              <input
+                type="text"
+                id="year"
+                name="year"
+                autoComplete="off"
+                required
+                className="usedInv-group"
+                value={usedInventoryAdd.year}
+                onChange={handleUsedInventoryAdd}
+              />
+              <label htmlFor="inputField" className="usedInv-label-name">
+                <span className="usedInv-content-name">Year</span>
+              </label>
+            </div>
 
-          <textarea type="car info" name="info"></textarea>
-        </form>
-     </Portal>: null}
+            <div className="usedInv-field">
+              <input
+                type="text"
+                id="make"
+                name="make"
+                autoComplete="off"
+                required
+                className="usedInv-group"
+                value={usedInventoryAdd.make}
+                onChange={handleUsedInventoryAdd}
+              />
+              <label htmlFor="inputField" className="usedInv-label-name">
+                <span className="usedInv-content-name">Make</span>
+              </label>
+            </div>
+
+            <div className="usedInv-field">
+              <input
+                type="text"
+                id="model"
+                name="model"
+                autoComplete="off"
+                required
+                className="usedInv-group"
+                value={usedInventoryAdd.model}
+                onChange={handleUsedInventoryAdd}
+              />
+              <label htmlFor="inputField" className="usedInv-label-name">
+                <span className="usedInv-content-name">Model</span>
+              </label>
+            </div>
+
+            <div className="usedInv-field">
+              <input
+                type="text"
+                id="price"
+                name="price"
+                autoComplete="off"
+                required
+                className="usedInv-group"
+                value={usedInventoryAdd.price}
+                onChange={handleUsedInventoryAdd}
+              />
+              <label htmlFor="inputField" className="usedInv-label-name">
+                <span className="usedInv-content-name">Price</span>
+              </label>
+            </div>
+
+            <div className="usedInv-field">
+              <input
+                type="text"
+                id="miles"
+                name="miles"
+                autoComplete="off"
+                required
+                className="usedInv-group"
+                value={usedInventoryAdd.miles}
+                onChange={handleUsedInventoryAdd}
+              />
+              <label htmlFor="inputField" className="usedInv-label-name">
+                <span className="usedInv-content-name">Miles</span>
+              </label>
+            </div>
+
+            <textarea type="car info" name="info"></textarea>
+
+            <Link className="usedInv-closeBtn" onClick={handleUsedInventoryCloseButton} to={props.user === "salesman" ? "/sales/dash" : "/dealer/inventory"}>
+                Close
+              </Link>
+
+            <button className="usedInv-submitBtn">Submit</button>
+          </form>
+        </Portal>
+      ) : null}
+      {showNewInventoryModal ? (
+        <Portal>
+          <form className="newInventoryAddForm">
+          <div className="newInv-field" id="picture_file">
+              <input
+                type="file"
+                id="car_picture"
+                name="car_picture"
+                className="newInv-group"
+                value={newInventoryAdd.car_picture}
+                onChange={handleNewInventoryAdd}
+              />
+              <label for="car_picture">Upload file</label>
+            </div>
+
+            <div className="newInv-field">
+              <input
+                type="text"
+                id="year"
+                name="year"
+                autoComplete="off"
+                required
+                className="newInv-group"
+                value={newInventoryAdd.year}
+                onChange={handleNewInventoryAdd}
+              />
+              <label htmlFor="inputField" className="newInv-label-name">
+                <span className="newInv-content-name">Year</span>
+              </label>
+            </div>
+
+            <div className="newInv-field">
+              <input
+                type="text"
+                id="make"
+                name="make"
+                autoComplete="off"
+                required
+                className="newInv-group"
+                value={newInventoryAdd.make}
+                onChange={handleNewInventoryAdd}
+              />
+              <label htmlFor="inputField" className="newInv-label-name">
+                <span className="newInv-content-name">Make</span>
+              </label>
+            </div>
+
+            <div className="newInv-field">
+              <input
+                type="text"
+                id="model"
+                name="model"
+                autoComplete="off"
+                required
+                className="newInv-group"
+                value={newInventoryAdd.model}
+                onChange={handleNewInventoryAdd}
+              />
+              <label htmlFor="inputField" className="newInv-label-name">
+                <span className="newInv-content-name">Model</span>
+              </label>
+            </div>
+
+            <div className="newInv-field">
+              <input
+                type="text"
+                id="price"
+                name="price"
+                autoComplete="off"
+                required
+                className="newInv-group"
+                value={newInventoryAdd.price}
+                onChange={handleNewInventoryAdd}
+              />
+              <label htmlFor="inputField" className="newInv-label-name">
+                <span className="newInv-content-name">Price</span>
+              </label>
+            </div>
+
+            <div className="newInv-field">
+              <input
+                type="text"
+                id="miles"
+                name="miles"
+                autoComplete="off"
+                required
+                className="newInv-group"
+                value={newInventoryAdd.miles}
+                onChange={handleNewInventoryAdd}
+              />
+              <label htmlFor="inputField" className="newInv-label-name">
+                <span className="newInv-content-name">Miles</span>
+              </label>
+            </div>
+
+            <textarea type="car info" name="info"></textarea>
+
+            <Link className="newInv-closeBtn" onClick={handleNewInventoryCloseButton} to={props.user === "salesman" ? "/sales/dash" : "/dealer/inventory"}>
+                Close
+              </Link>
+
+            <button className="newInv-submitBtn">Submit</button>
+          </form>
+        </Portal>
+      ) : null}
       <div className="header-dealer">
         <div className="header-dealer__name">Dealership: Ford</div>
         <div className="header-dealer__notifications">Notifications</div>
@@ -123,12 +351,19 @@ const DealerInventoryMain = (props) => {
           </form>
         </div>
         <div className="new--inventory__results">
-          <h2>
-            No New Inventory, <Link onClick={handleNewInventoryAdd} to="/dealer/inventory/NewInventory">Click Here To Add</Link>{" "}
+          <h3>
+            No New Inventory, Click{" "}
+            <Link
+              onClick={handleNewInventoryAdd}
+              to="/dealer/inventory/NewInventory"
+            >
+              <span className="newInventoryAddLink">Here</span>{" "}
+            </Link>
+            To Add{" "}
             <span role="img" aria-label="crying">
               😢
             </span>
-          </h2>
+          </h3>
         </div>
       </div>
 
@@ -186,16 +421,29 @@ const DealerInventoryMain = (props) => {
           </form>
         </div>
         <div className="used--inventory__results">
-          <h2>
-            No Used Inventory, <Link onClick={handleUsedInventoryAdd} to="/dealer/inventory/UsedInventory">Click Here To Add</Link>{" "}
+          <h3>
+            No Used Inventory, Click{" "}
+            <Link
+              onClick={handleUsedInventoryAdd}
+              to="/dealer/inventory/UsedInventory"
+            >
+              <span className="usedInventoryAddLink"> Here </span>{" "}
+            </Link>
+            To Add{" "}
             <span role="img" aria-label="crying">
               😢
             </span>
-          </h2>
+          </h3>
         </div>
       </div>
     </>
   );
 };
 
-export default DealerInventoryMain;
+const mapStateToProps = (state) => {
+  return {
+    user: state.userReducer.user
+  }
+}
+
+export default connect(mapStateToProps, {})(DealerInventoryMain);

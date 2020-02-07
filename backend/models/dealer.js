19e6e477e8c3d2182c4207b1e8e388441f46caf0
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-06 22:03:02 -0500

    :sparkles: adds dealer model
    adds dealer model

---
+++ backend/models/dealer.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Dealer = sequelize.define(
+    "Dealer",
+    {
+      dealer_username: DataTypes.STRING,
+      dealer_email: DataTypes.STRING,
+      dealer_password: DataTypes.STRING,
+      dealer_name: DataTypes.STRING,
+      dealer_street: DataTypes.STRING,
+      dealer_city: DataTypes.STRING,
+      dealer_state: DataTypes.STRING,
+      dealer_country: DataTypes.STRING,
+      dealer_zipcode: DataTypes.STRING
+    },
+    {}
+  );
+  Dealer.associate = function(models) {
+    // associations can be defined here
+    console.log("MY MODELS", models);
+    Dealer.hasMany(models.Lead);
+    Dealer.hasMany(models.Salesman);
+  };
+  return Dealer;
+};

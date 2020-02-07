03cb416cc57ab299cb7d314aa4004292a0606ecd
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-06 23:39:55 -0500

    :sparkles: adds dotenv variables for dev
    adds dotenv variables for dev

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,27 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "dealer_crm",
-    host: "localhost",
-    dialect: "postgres",
-    operatorsAliases: false
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: false
+    dialect: "mysql"
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: false
+    dialect: "mysql"
   }
 };

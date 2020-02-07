a973b520d82a11a0130cf3074a975d574327e81d
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-07 17:22:28 -0500

    :sparkles: adds routers to server and cors()
    adds routers to server and cors()

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+
 const server = express();
 
+server.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
 // Routes
 const dealerRouter = require("./Dealers/DealerRoutes");
+const leadsRouter = require("./Leads/LeadsRoutes");
+const salesmanRouter = require("./Salesman/SalesmanRoutes");
 
 server.use(express.json());
 server.use("/api/dealer", dealerRouter);
+server.use("/api/leads", leadsRouter);
+server.use("/api/sales", salesmanRouter);
 
 module.exports = server;

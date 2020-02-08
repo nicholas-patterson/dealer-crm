231cfb1150a84ac47435d69f4afc52a0212495de
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-07 23:37:04 -0500

    :sparkles: adds sales login reducer
    adds sales login reducer

---
+++ src/reducers/salesLoginReducer.js
@@ -0,0 +1,53 @@
+const initialState = {
+  isLogged: false,
+  user: [],
+  //token: null,
+  error: ""
+};
+
+export const salesLoginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LOGIN_SALES_USER_START":
+      return {
+        ...state,
+        isLogged: false
+      };
+    case "LOGIN_SALES_USER_SUCCESS":
+      return {
+        ...state,
+        isLogged: true,
+        user: action.payload
+        // token: action.payload.token
+      };
+
+    case "LOGIN_SALES_USER_FAILURE":
+      return {
+        ...state,
+        isLogged: false,
+        error: "Login Failed"
+      };
+
+    case "LOGOUT_USER_START":
+      return {
+        ...state,
+        isLogged: true
+        //token: action.payload
+      };
+    case "LOGOUT_USER_SUCCESS":
+      return {
+        ...state,
+        isLogged: false
+        //token: localStorage.clear()
+      };
+
+    case "LOGOUT_USER_FAILURE":
+      return {
+        ...state,
+        isLogged: false,
+        error: ""
+      };
+
+    default:
+      return state;
+  }
+};

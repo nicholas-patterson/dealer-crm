dc9fea9b8637d876f108594103aae59f5518f4f1
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-09 19:22:28 -0500

    :sparkles: adds lead_type to models
    adds lead_type to models

--- backend/models/lead.js
+++ backend/models/lead.js
@@ -1,18 +1,15 @@
-"use strict";
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Lead = sequelize.define(
-    "Lead",
-    {
-      lead_firstname: DataTypes.STRING,
-      lead_lastname: DataTypes.STRING,
-      lead_street: DataTypes.STRING,
-      lead_city: DataTypes.STRING,
-      lead_state: DataTypes.STRING,
-      lead_email: DataTypes.STRING,
-      lead_phone: DataTypes.STRING
-    },
-    {}
-  );
+  const Lead = sequelize.define('Lead', {
+    lead_firstname: DataTypes.STRING,
+    lead_lastname: DataTypes.STRING,
+    lead_street: DataTypes.STRING,
+    lead_city: DataTypes.STRING,
+    lead_state: DataTypes.STRING,
+    lead_email: DataTypes.STRING,
+    lead_phone: DataTypes.STRING,
+    lead_type: DataTypes.STRING
+  }, {});
   Lead.associate = function(models) {
     // associations can be defined here
   };

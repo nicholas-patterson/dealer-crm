39f07574004320af4957e0ca43087318b39fce02
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-12 17:07:44 -0500

    :sparkles: adds edit leads route
    adds edit leads route

--- backend/Leads/LeadsRoutes.js
+++ backend/Leads/LeadsRoutes.js
@@ -53,6 +53,38 @@ router.post("/add", async (req, res) => {
   }
 });
 
+// Edit Lead
+router.put("/update/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [rowsUpdated, [updatedBook]] = await db.Lead.update(
+      {
+        lead_firstname: req.body.lead_firstname,
+        lead_lastname: req.body.lastname,
+        lead_street: req.body.lead_street,
+        lead_city: req.body.lead_city,
+        lead_state: req.body.lead_state,
+        lead_email: req.body.lead_email,
+        lead_phone: req.body.lead_phone,
+        lead_type: req.body.lead_type,
+        dealer_id: req.session.dealer_user.id
+      },
+      { returning: true, where: { id } }
+    );
+    console.log("ROWS UDATED", rowsUpdated);
+    if (rowsUpdated === 0) {
+      res
+        .status(400)
+        .json({ warning: "Lead with that id was not found and or updated" });
+    } else {
+      res.status(200).json({ rowsUpdated, updatedBook });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Delete Lead
 router.delete("/remove/:id", (req, res) => {
   const { id } = req.params;
@@ -85,36 +117,5 @@ router.delete("/remove/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
-
-// router.delete("/remove/:id", async (req, res) => {
-//   const { id } = req.params;
-
-//   try {
-//     const lead = await db.Lead.findOne({
-//       where: {
-//         id
-//       }
-//     });
-
-//     const deletedLead = await db.Lead.destroy({
-//       where: {
-//         id: lead.id
-//       }
-//     });
-
-//     console.log("LEAD BELOW", lead);
-
-//     if (lead == null) {
-//       res.status(400).json({ warning: "Lead not found" });
-//     } else {
-//       res.status(200).json({ deletedLead: lead });
-//     }
-
-//     //console.log("LEAD FOUND", lead);
-//     //console.log("BETTER LEAD FOUND", lead.get({ plain: true }));
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;

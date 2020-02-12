a9fd71240976682ec865101a632476288f3ff381
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-12 16:57:14 -0500

    :recycle: combines edit lead and delete lead into one reducer
    combines edit lead and delete lead into one reducer

--- src/reducers/getDealerLeadReducer.js
+++ src/reducers/getDealerLeadReducer.js
@@ -23,6 +23,27 @@ export const getDealerLeadReducer = (state = initalState, action) => {
         isLoading: false,
         error: action.payload
       };
+    case "EDIT_LEAD_START":
+      return {
+        ...state,
+        isLoading: true
+      };
+
+    case "EDIT_LEAD_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        leads: state.leads.map(item =>
+          item.id === action.payload.id ? action.payload : item
+        )
+      };
+
+    case "EDIT_LEAD_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
     case "DELETE_LEAD_START":
       return {
         ...state,

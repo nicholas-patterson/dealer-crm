1717d30bc63a1291b6bdf395bfae67e0a68675ca
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-12 16:58:26 -0500

    :fire: commeneted out editLeadReducer file
    commeneted out editLeadReducer file

--- src/reducers/editLeadReducer.js
+++ src/reducers/editLeadReducer.js
@@ -1,33 +1,33 @@
-const initialState = {
-  updatedLead: {},
-  isLoading: false,
-  //token: localStorage.getItem("token"),
-  error: ""
-};
+// const initialState = {
+//   updatedLead: {},
+//   isLoading: false,
+//   //token: localStorage.getItem("token"),
+//   error: ""
+// };
 
-export const editLeadReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "EDIT_LEAD_START":
-      return {
-        ...state,
-        isLoading: true
-        //token: localStorage.getItem("token")
-      };
+// export const editLeadReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "EDIT_LEAD_START":
+//       return {
+//         ...state,
+//         isLoading: true
+//         //token: localStorage.getItem("token")
+//       };
 
-    case "EDIT_LEAD_SUCCESS":
-      return {
-        ...state,
-        isLoading: false,
-        updatedLead: action.payload
-      };
+//     case "EDIT_LEAD_SUCCESS":
+//       return {
+//         ...state,
+//         isLoading: false,
+//         updatedLead: action.payload
+//       };
 
-    case "EDIT_LEAD_FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload
-      };
-    default:
-      return state;
-  }
-};
+//     case "EDIT_LEAD_FAILURE":
+//       return {
+//         ...state,
+//         isLoading: false,
+//         error: action.payload
+//       };
+//     default:
+//       return state;
+//   }
+// };

ac0f5e06899f2a6a03b1088c0249752e5acafa6f
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-13 21:58:23 -0500

    :sparkles: adds other part of lead relation
    adds other part of lead relation

--- backend/models/lead.js
+++ backend/models/lead.js
@@ -1,17 +1,24 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Lead = sequelize.define('Lead', {
-    lead_firstname: DataTypes.STRING,
-    lead_lastname: DataTypes.STRING,
-    lead_street: DataTypes.STRING,
-    lead_city: DataTypes.STRING,
-    lead_state: DataTypes.STRING,
-    lead_email: DataTypes.STRING,
-    lead_phone: DataTypes.STRING,
-    lead_type: DataTypes.STRING
-  }, {});
+  const Lead = sequelize.define(
+    "Lead",
+    {
+      lead_firstname: DataTypes.STRING,
+      lead_lastname: DataTypes.STRING,
+      lead_street: DataTypes.STRING,
+      lead_city: DataTypes.STRING,
+      lead_state: DataTypes.STRING,
+      lead_email: DataTypes.STRING,
+      lead_phone: DataTypes.STRING,
+      lead_type: DataTypes.STRING
+    },
+    {}
+  );
   Lead.associate = function(models) {
     // associations can be defined here
+    Lead.belongsTo(models.Dealer, {
+      foreignKey: "dealer_id"
+    });
   };
   return Lead;
 };

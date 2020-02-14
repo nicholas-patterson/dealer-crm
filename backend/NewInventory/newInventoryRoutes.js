eeedad34c781b92590e0f89ba9b4066ece900c58
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-13 22:02:53 -0500

    :sparkles: adds newInventory routes
    adds newInventory routes

---
+++ backend/NewInventory/newInventoryRoutes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const db = require("../models");
+
+// Get All New Inventory
+router.get("/", async (req, res) => {
+  try {
+    const inventory = await db.NewInventory.findAll();
+    res.status(200).json(inventory);
+  } catch (err) {
+    res.status(500).json(er);
+  }
+});
+
+// Get All New Inventory By Id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const inventory = await db.NewInventory.findOne({
+      where: {
+        id
+      }
+    });
+    res.status(200).json(inventory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add New Inventory By Dealer ID
+
+router.post("/add", async (req, res) => {
+  try {
+    const newInventory = await db.NewInventory.create({
+      year: req.body.year,
+      make: req.body.mmake,
+      model: req.body.model,
+      price: req.body.price,
+      miles: req.body.miles,
+      info: req.body.info,
+      dealer_id: req.session.dealer_user.id,
+      image_id: req.session.image.id
+    });
+    res.status(201).json(newInventory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

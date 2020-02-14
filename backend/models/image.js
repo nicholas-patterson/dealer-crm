6a2a8ead1b03e306fa7f047ebce8b147f8428f04
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-13 22:00:12 -0500

    :sparkles: adds association for image table
    adds association for image table

--- backend/models/image.js
+++ backend/models/image.js
@@ -1,11 +1,29 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Image = sequelize.define('Image', {
-    car_picture: DataTypes.STRING,
-    imageId: DataTypes.STRING
-  }, {});
+  const Image = sequelize.define(
+    "Image",
+    {
+      car_picture: DataTypes.STRING,
+      cloudinary_pic_id: DataTypes.STRING,
+      dealer_id: DataTypes.INTEGER
+    },
+    {}
+  );
   Image.associate = function(models) {
     // associations can be defined here
+    Image.hasMany(models.Inventory, {
+      foreignKey: "image_id"
+    });
+
+    Image.hasMany(models.NewInventory, {
+      foreignKey: "image_id"
+    });
+
+    // Image.belongsToMany(models.Inventory, {
+    //   through: models.InventoryImage,
+    //   foreignKey: "image_id",
+    //   otherKey: "inventory_id"
+    // });
   };
   return Image;
 };

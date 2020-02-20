298358c7197c717a5087bc9feec498e262fa8aae
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-20 15:45:21 -0500

    :sparkles: adds lazy and suspense fo loading
    adds lazy and suspense fo loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { connect } from "react-redux";
+import { Router } from "@reach/router";
 import WelcomePage from "./components/WelcomePage";
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LogInForm";
 import SalesLoginForm from "./components/SalesLoginForm";
-import DealerDashBoard from "./components/Dashboards/DealerDashboard";
-import DealerDashboardMain from "./components/SwitchItems/DealerDashboardMain";
-import DealerLeadsMain from "./components/SwitchItems/DealerLeadsMain";
-import DealerInventoryMain from "./components/SwitchItems/DealerInventoryMain";
-import DealerHelpMain from "./components/SwitchItems/DealerHelpMain";
-import DealerAccountMain from "./components/SwitchItems/DealerAccountMain";
-import { connect } from "react-redux";
+// Import for lazy load
 
-import { Router } from "@reach/router";
+const DealerDashBoard = lazy(() =>
+  import("./components/Dashboards/DealerDashboard")
+);
+const DealerDashboardMain = lazy(() =>
+  import("./components/SwitchItems/DealerDashboardMain")
+);
+const DealerLeadsMain = lazy(() =>
+  import("./components/SwitchItems/DealerLeadsMain")
+);
+const DealerInventoryMain = lazy(() =>
+  import("./components/SwitchItems/DealerInventoryMain")
+);
+const DealerHelpMain = lazy(() =>
+  import("./components/SwitchItems/DealerHelpMain")
+);
+const DealerAccountMain = lazy(() =>
+  import("./components/SwitchItems/DealerAccountMain")
+);
+
+// import DealerDashBoard from "./components/Dashboards/DealerDashboard";
+// import DealerDashboardMain from "./components/SwitchItems/DealerDashboardMain";
+// import DealerLeadsMain from "./components/SwitchItems/DealerLeadsMain";
+// import DealerInventoryMain from "./components/SwitchItems/DealerInventoryMain";
+// import DealerHelpMain from "./components/SwitchItems/DealerHelpMain";
+// import DealerAccountMain from "./components/SwitchItems/DealerAccountMain";
 
 const App = props => {
   console.log("PROPS IN APP.JS", props);
@@ -24,24 +44,19 @@ const App = props => {
         <SalesLoginForm path="/saleslogin" />
       </Router>
       {props.user === "dealer" ? (
-        <Router>
-          {/* <WelcomePage default="/" />
-          <SignUpForm path="/signup" />
-          <LoginForm path="/login" />
-          <SalesLoginForm path="/saleslogin" /> */}
-          <DealerDashBoard path="/dealer">
-            <DealerDashboardMain path="/dash" />
-            <DealerLeadsMain path="/leads" />
-            <DealerInventoryMain path="/inventory" />
-            <DealerHelpMain path="/help" />
-            <DealerAccountMain path="/account" />
-          </DealerDashBoard>
-        </Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <DealerDashBoard path="/dealer">
+              <DealerDashboardMain path="/dash" />
+              <DealerLeadsMain path="/leads" />
+              <DealerInventoryMain path="/inventory" />
+              <DealerHelpMain path="/help" />
+              <DealerAccountMain path="/account" />
+            </DealerDashBoard>
+          </Router>
+        </Suspense>
       ) : (
         <Router>
-          {/* <WelcomePage default="/" />
-          <SignUpForm path="/signup" />
-          <LoginForm path="/login" /> */}
           <DealerDashBoard path="/sales">
             <DealerDashboardMain path="/dash" />
             <DealerLeadsMain path="/leads" />

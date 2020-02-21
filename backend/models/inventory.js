e056bef6c26d74b094664841ed6fbc792213b681
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-21 18:24:09 -0500

    :sparkles: adds onDelete cascade for fk
    adds onDelete cascade for fk

--- backend/models/inventory.js
+++ backend/models/inventory.js
@@ -18,11 +18,13 @@ module.exports = (sequelize, DataTypes) => {
   Inventory.associate = function(models) {
     //associations can be defined here
     Inventory.belongsTo(models.Dealer, {
-      foreignKey: "dealer_id"
+      foreignKey: "dealer_id",
+      onDelete: "cascade"
     });
 
     Inventory.belongsTo(models.Image, {
-      foreignKey: "image_id"
+      foreignKey: "image_id",
+      onDelete: "cascade"
     });
   };
   return Inventory;

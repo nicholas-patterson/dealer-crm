251e36cff11e493f1b89a835dd8a467b55be33d4
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-21 18:27:55 -0500

    :sparkles: adds cases for image delete
    adds cases for image delete

--- src/reducers/imageReducer.js
+++ src/reducers/imageReducer.js
@@ -45,6 +45,30 @@ export const imageReducer = (state = initialState, action) => {
         loading: false,
         error: action.payload
       };
+
+    case "DELETE_IMAGE_START":
+      return {
+        ...state,
+        loading: true
+      };
+
+    case "DELETE_IMAGE_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        images: state.images.filter(img => {
+          console.log("IMG IN REDUCER", img);
+          console.log("ACTION PAYLOAD IN IMG REDUCER", action.payload);
+          return img.id !== action.payload.id;
+        })
+      };
+
+    case "DELETE_IMAGE_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
     default:
       return state;
   }

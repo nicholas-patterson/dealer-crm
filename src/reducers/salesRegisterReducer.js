76f16c13846b843e70e935fc4123e1d4b7ebfb81
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-02-27 15:17:52 -0500

    :sparkles: adds salesRegisterReducer
    adds salesRegisterReducer

---
+++ src/reducers/salesRegisterReducer.js
@@ -0,0 +1,34 @@
+// REGISTER REDUCER FOR SALESMAN
+
+const initialState = {
+  user: [],
+  isRegistering: false,
+  error: []
+};
+
+export const salesRegisterReducer = (state = initialState, action) => {
+  console.log(action.payload);
+  switch (action.type) {
+    case "REG_SALESMAN_START":
+      return {
+        ...state,
+        isRegistering: true
+      };
+    case "REG_SALESMAN_SUCCESS":
+      return {
+        ...state,
+        isRegistering: false,
+        user: action.payload
+      };
+
+    case "REG_SALESMAN_FAILURE":
+      return {
+        ...state,
+        isRegistering: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};

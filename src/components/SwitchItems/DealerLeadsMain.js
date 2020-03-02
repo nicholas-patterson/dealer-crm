07062db36a9017a253f83a44d86bbe12b56531e8
= <nicholaspatterson36@gmail.com>
2020-03-01 19:14:26 -0500

    :bug: conditional rendered useEffect and salesmanleads component
    conditional rendered useEffect and salesmanleads component

--- src/components/SwitchItems/DealerLeadsMain.js
+++ src/components/SwitchItems/DealerLeadsMain.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { getDealerLeads, deleteLead, editLead } from "../../actions/index";
+import {
+  getDealerLeads,
+  deleteLead,
+  editLead,
+  getSalesmanLeads
+} from "../../actions/index";
 import DealerSingleLeadMain from "./DealerSingleLeadMain";
+import SalesmanSingleLeadMain from "./SalesmanSingleLeadMain";
 
 const DealerLeadsMain = props => {
+  console.log("SALES LEADS", props);
   useEffect(() => {
-    props.getDealerLeads();
+    props.user === "salesman"
+      ? props.getSalesmanLeads()
+      : props.getDealerLeads();
   }, []);
 
   const handleLeadDelete = id => {
@@ -50,20 +59,36 @@ const DealerLeadsMain = props => {
           </thead>
           <td className="spaceunder"></td>
           <tbody>
-            {props.leads &&
-              props.leads.map((lead, index) => {
-                return (
-                  <DealerSingleLeadMain
-                    index={index}
-                    key={lead.id}
-                    lead={lead}
-                    handleLeadDelete={handleLeadDelete}
-                    user={props.user}
-                    props={props}
-                    leads={props.leads}
-                  />
-                );
-              })}
+            {props.user === "dealer"
+              ? props.leads &&
+                props.leads.map((lead, index) => {
+                  console.log("DEALER LEAD IN MAP", lead);
+                  return (
+                    <DealerSingleLeadMain
+                      index={index}
+                      key={lead.id}
+                      lead={lead}
+                      handleLeadDelete={handleLeadDelete}
+                      user={props.user}
+                      props={props}
+                      leads={props.leads}
+                    />
+                  );
+                })
+              : props.sales_leads &&
+                props.sales_leads.map((lead, index) => {
+                  console.log("SALESMAN LEADS IN MAP", lead);
+                  return (
+                    <SalesmanSingleLeadMain
+                      index={index}
+                      key={lead.id}
+                      lead={lead}
+                      user={props.user}
+                      props={props}
+                      leads={props.sales_leads}
+                    />
+                  );
+                })}
           </tbody>
         </table>
       </div>
@@ -72,14 +97,17 @@ const DealerLeadsMain = props => {
 };
 
 const mapStateToProps = state => {
+  console.log("STATE STATE STATE", state);
   return {
     user: state.userReducer.user,
-    leads: state.getDealerLeadReducer.leads
+    leads: state.getDealerLeadReducer.leads,
+    sales_leads: state.getSalesmanLeadReducer.leads
   };
 };
 
 export default connect(mapStateToProps, {
   getDealerLeads,
   deleteLead,
-  editLead
+  editLead,
+  getSalesmanLeads
 })(DealerLeadsMain);

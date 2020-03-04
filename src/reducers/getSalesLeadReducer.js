1ac6e28b48d93809411dd9c1822bdd15aa1954a2
= <nicholaspatterson36@gmail.com>
2020-03-03 23:55:33 -0500

    :sparkles: adds reducer to get sales leads and crud them
    adds reducer to get sales leads and crud them

---
+++ src/reducers/getSalesLeadReducer.js
@@ -0,0 +1,68 @@
+const initialState = {
+  leads: [],
+  isLoading: false,
+  error: ""
+};
+
+export const getSalesLeadReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_SALES_LEADS_START":
+      return {
+        ...state,
+        isLoading: true
+      };
+    case "GET_SALES_LEADS_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        leads: action.payload
+      };
+    case "GET_SALES_LEADS_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case "ADD_SALES_LEAD_START":
+      return {
+        ...state,
+        isLoading: true
+      };
+    case "ADD_SALES_LEAD_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        leads: [...state.leads, action.payload]
+      };
+
+    case "ADD_SALES_LEAD_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case "DELETE_SALESMAN_LEAD_START":
+      return {
+        ...state,
+        isLoading: true
+      };
+
+    case "DELETE_SALESMAN_LEAD_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        leads: state.leads.filter(lead => lead.id != action.payload.id)
+      };
+
+    case "DELETE_SALESMAN_LEAD_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};

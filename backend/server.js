89396c06cec9125c0f6bb0c03e92e68720b6640c
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-10 05:16:31 -0400

    :art: writing comments on code to get an idea of how notifications would work
    writing comments on code to get an idea of how notifications would work

--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,53 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const server = express();
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io").listen(server);
+app.set("socketio", io); // SET IO INSTANCE TO SOCKETIO TO USE IN OTHER ROUTES IF NEED BE
 const redis = require("redis");
 const client = redis.createClient();
 const session = require("express-session");
 const RedisStore = require("connect-redis")(session);
 
-server.use(
+app.use(
   cors({
     credentials: true,
     origin: "http://localhost:3000"
   })
 );
 
-server.use(
-  session({
-    secret: process.env.REDIS_SECRET,
-    name: "_dealerCRM",
-    saveUninitialized: false,
-    resave: false,
-    store: new RedisStore({
-      host: "localhost",
-      port: 6379,
-      client: client,
-      ttl: 260
-    })
+// CREATE SESSION STORE
+const sessionMiddleware = session({
+  secret: process.env.REDIS_SECRET,
+  name: "_dealerCRM",
+  saveUninitialized: false,
+  resave: false,
+  store: new RedisStore({
+    host: "localhost",
+    port: 6379,
+    client: client,
+    ttl: 260
   })
-);
+});
+
+// WHENEVER A SESSION IS CREATED (ON LOGIN) A SOCKET IS CREATED WITH THAT SESSSION
+io.use(function(socket, next) {
+  sessionMiddleware(socket.request, {}, next);
+});
+
+app.use(sessionMiddleware);
+
+// IO
+io.on("connection", (socket, req) => {
+  console.log("SOCKET", socket); // SOCKET INFORMATION ALONG WITH SESSION (CHECK DEALER LOGIN ROUTE WHERE SESSION IS CREATED)
+  // Save all connected sockets in array or in redis (stack overflow suggestion)
+
+  socket.on("disconnect", () => {
+    console.log(`Socket ${socket.id} disconnected.`);
+  });
+});
 
 // Routes
 const dealerRouter = require("./Dealers/DealerRoutes");
@@ -38,12 +58,12 @@ const imageRouter = require("./Images/ImageRoutes");
 const usedInventoryRouter = require("./UsedInventory/InventoryRoutes");
 const newInventoryRouter = require("./NewInventory/newInventoryRoutes");
 
-server.use(express.json());
-server.use("/api/dealer", dealerRouter);
-server.use("/api/leads", leadsRouter);
-server.use("/api/sales", salesmanRouter);
-server.use("/api/image", imageRouter);
-server.use("/api/usedInventory", usedInventoryRouter);
-server.use("/api/newInventory", newInventoryRouter);
+app.use(express.json());
+app.use("/api/dealer", dealerRouter);
+app.use("/api/leads", leadsRouter);
+app.use("/api/sales", salesmanRouter);
+app.use("/api/image", imageRouter);
+app.use("/api/usedInventory", usedInventoryRouter);
+app.use("/api/newInventory", newInventoryRouter);
 
 module.exports = server;

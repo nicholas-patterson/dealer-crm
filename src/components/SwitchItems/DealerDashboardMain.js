6defc5cfe2b2fbf5a37b6269c34812a73b39dcef
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-15 19:28:31 -0400

    :sparkles: adds socketio-client to get notifications have to save to state
    adds socketio-client to get notifications have to save to state

--- src/components/SwitchItems/DealerDashboardMain.js
+++ src/components/SwitchItems/DealerDashboardMain.js
@@ -1,220 +1,246 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "@reach/router";
 import { connect } from "react-redux";
 import { getSalesmans } from "../../actions/index";
 
+import io from "socket.io-client";
+
 const DealerDashboardMain = props => {
-  console.log("DEALERDASHMAIN", props);
+  const [notifications, setNotifications] = useState([]);
+
+  const socket = io("http://localhost:5000", {
+    transports: ["websocket"],
+    multiplex: false,
+    query: `token=${
+      props.user === "dealer" ? props.dealer_token : props.salesman_token
+    }`
+  });
+
+  useEffect(() => {
+    socket.on("message", message => {
+      console.log("FROM BACKEND", message);
+      setNotifications([...notifications, message.data.message]);
+    });
+  }, [props.user === "dealer" ? props.dealer_leads : props.salesman_leads]);
+
+  console.log("NOTIFICATIONS", notifications);
+
   return (
     <>
-      {props.user === "salesman" ? (
-        <div className="header-dealer">
-          <div className="header-dealer__name">
-            Welcome, {props.salesman.salesman_username}
+      <div>
+        {props.user === "salesman" ? (
+          <div className="header-dealer">
+            <div className="header-dealer__name">
+              Welcome, {props.salesman.salesman_username}
+            </div>
+            <div className="header-dealer__notifications">Notifications</div>
           </div>
-          <div className="header-dealer__notifications">Notifications</div>
-        </div>
-      ) : (
-        <div className="header-dealer">
-          <div className="header-dealer__name">Dealership: Ford</div>
-          <div className="header-dealer__notifications">Notifications</div>
-        </div>
-      )}
-      {/* </div> */}
-      {/* ACTION AND RECENT ACTIVITY BOXES */}
-      <div className="console-recent-activity-container">
-        {/* START CONSOLE */}
-        <div className="console">
-          <h2
-            className={
-              "console__heading " +
-              (props.user === "salesman" ? "sales_bb" : "dealer_bb")
-            }
-          >
-            what would you like to do?
-          </h2>
-
-          <div className="console__controls">
-            <Link
-              to={
-                props.user === "salesman"
-                  ? "/sales/dash/newlead"
-                  : "/dealer/dash/newlead"
-              }
-              //to="/dealer/dash/newlead"
+        ) : (
+          <div className="header-dealer">
+            <div className="header-dealer__name">Dealership: Ford</div>
+            <div className="header-dealer__notifications">Notifications</div>
+          </div>
+        )}
+        {/* </div> */}
+        {/* ACTION AND RECENT ACTIVITY BOXES */}
+        <div className="console-recent-activity-container">
+          {/* START CONSOLE */}
+          <div className="console">
+            <h2
               className={
-                "console__control " +
-                (props.user === "salesman" ? "sales_border" : "dealer_border")
+                "console__heading " +
+                (props.user === "salesman" ? "sales_bb" : "dealer_bb")
               }
-              onClick={props.leadsClick}
             >
-              <p>Add New Lead</p>
-            </Link>
+              what would you like to do?
+            </h2>
 
-            {props.user === "salesman" ? null : (
+            <div className="console__controls">
               <Link
-                to="/dealer/dash/newsalesperson"
+                to={
+                  props.user === "salesman"
+                    ? "/sales/dash/newlead"
+                    : "/dealer/dash/newlead"
+                }
+                //to="/dealer/dash/newlead"
                 className={
                   "console__control " +
                   (props.user === "salesman" ? "sales_border" : "dealer_border")
                 }
-                //onClick={props.salesClick}
-                onClick={getSalesmans()}
+                onClick={props.leadsClick}
               >
-                <p>Add New Salesperson</p>
+                <p>Add New Lead</p>
               </Link>
-            )}
 
-            {props.user === "salesman" ? null : (
-              <Link
-                to="/dealer/dash/newmanager"
-                className={
-                  "console__control " +
-                  (props.user === "salesman" ? "sales_border" : "dealer_border")
-                }
-                onClick={props.managerClick}
-              >
-                <p>Add New Manager</p>
-              </Link>
-            )}
+              {props.user === "salesman" ? null : (
+                <Link
+                  to="/dealer/dash/newsalesperson"
+                  className={
+                    "console__control " +
+                    (props.user === "salesman"
+                      ? "sales_border"
+                      : "dealer_border")
+                  }
+                  //onClick={props.salesClick}
+                  onClick={getSalesmans()}
+                >
+                  <p>Add New Salesperson</p>
+                </Link>
+              )}
+
+              {props.user === "salesman" ? null : (
+                <Link
+                  to="/dealer/dash/newmanager"
+                  className={
+                    "console__control " +
+                    (props.user === "salesman"
+                      ? "sales_border"
+                      : "dealer_border")
+                  }
+                  onClick={props.managerClick}
+                >
+                  <p>Add New Manager</p>
+                </Link>
+              )}
+            </div>
           </div>
-        </div>
 
-        {/* START RECENT ACTIVITY */}
-        <div className="recent-activity">
-          <h2
-            className={
-              "recent-activity__heading " +
-              (props.user === "salesman" ? "sales_bb" : "dealer_bb")
-            }
-          >
-            recent activity{" "}
-            <span
+          {/* START RECENT ACTIVITY */}
+          <div className="recent-activity">
+            <h2
               className={
-                "iconify recent-activity__help-icon " +
-                (props.user === "salesman"
-                  ? "sales_help_icon"
-                  : "dealer_help_icon")
+                "recent-activity__heading " +
+                (props.user === "salesman" ? "sales_bb" : "dealer_bb")
               }
-              data-icon="mdi:map-marker-question-outline"
-              data-inline="false"
-            ></span>
-          </h2>
-          <div className="recent-activity__items">
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:account-badge-alert-outline"
-                  data-inline="false"
-                ></span>
+            >
+              recent activity{" "}
+              <span
+                className={
+                  "iconify recent-activity__help-icon " +
+                  (props.user === "salesman"
+                    ? "sales_help_icon"
+                    : "dealer_help_icon")
+                }
+                data-icon="mdi:map-marker-question-outline"
+                data-inline="false"
+              ></span>
+            </h2>
+            <div className="recent-activity__items">
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:account-badge-alert-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new lead
+                </div>
+                <div className="recent-activity__item__time">Just now</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new lead
-              </div>
-              <div className="recent-activity__item__time">Just now</div>
-            </div>
 
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:alert-rhombus-outline"
-                  data-inline="false"
-                ></span>
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:alert-rhombus-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new salesperson
+                </div>
+                <div className="recent-activity__item__time">1 hour ago</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new salesperson
-              </div>
-              <div className="recent-activity__item__time">1 hour ago</div>
-            </div>
 
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:alert-rhombus-outline"
-                  data-inline="false"
-                ></span>
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:alert-rhombus-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new manager
+                </div>
+                <div className="recent-activity__item__time">3 hours ago</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new manager
-              </div>
-              <div className="recent-activity__item__time">3 hours ago</div>
-            </div>
 
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:alert-circle-outline"
-                  data-inline="false"
-                ></span>
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:alert-circle-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new vehicle
+                </div>
+                <div className="recent-activity__item__time">4 hours ago</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new vehicle
-              </div>
-              <div className="recent-activity__item__time">4 hours ago</div>
-            </div>
 
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:alert-circle-outline"
-                  data-inline="false"
-                ></span>
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:alert-circle-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new vehicle
+                </div>
+                <div className="recent-activity__item__time">4 hours ago</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new vehicle
-              </div>
-              <div className="recent-activity__item__time">4 hours ago</div>
-            </div>
 
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:account-badge-alert-outline"
-                  data-inline="false"
-                ></span>
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:account-badge-alert-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new lead
+                </div>
+                <div className="recent-activity__item__time">6 hours ago</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new lead
-              </div>
-              <div className="recent-activity__item__time">6 hours ago</div>
-            </div>
 
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:account-badge-alert-outline"
-                  data-inline="false"
-                ></span>
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:account-badge-alert-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new lead
+                </div>
+                <div className="recent-activity__item__time">6 hours ago</div>
               </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new lead
+              <div className="recent-activity__item">
+                <div className="recent-activity__item__box recent-activity__item__icon">
+                  <span
+                    className="iconify"
+                    data-icon="mdi:account-badge-alert-outline"
+                    data-inline="false"
+                  ></span>
+                </div>
+                <div className="recent-activity__item__box recent-activity__item__text">
+                  You added a new lead
+                </div>
+                <div className="recent-activity__item__time">6 hours ago</div>
               </div>
-              <div className="recent-activity__item__time">6 hours ago</div>
             </div>
-            <div className="recent-activity__item">
-              <div className="recent-activity__item__box recent-activity__item__icon">
-                <span
-                  className="iconify"
-                  data-icon="mdi:account-badge-alert-outline"
-                  data-inline="false"
-                ></span>
-              </div>
-              <div className="recent-activity__item__box recent-activity__item__text">
-                You added a new lead
-              </div>
-              <div className="recent-activity__item__time">6 hours ago</div>
-            </div>
+            {/* END RECENT ACTIVITY */}
+
+            {/* END ACTION AND RECENT ACTIVITY BOXES */}
           </div>
-          {/* END RECENT ACTIVITY */}
-
-          {/* END ACTION AND RECENT ACTIVITY BOXES */}
         </div>
       </div>
     </>
@@ -224,7 +250,11 @@ const DealerDashboardMain = props => {
 const mapStateToProps = state => {
   return {
     user: state.userReducer.user,
-    salesman: state.salesLoginReducer.user
+    salesman: state.salesLoginReducer.user,
+    dealer_leads: state.getDealerLeadReducer.leads,
+    salesman_leads: state.getSalesLeadReducer.leads,
+    dealer_token: state.loginReducer.user.token,
+    salesman_token: state.salesLoginReducer.user.token
   };
 };
 

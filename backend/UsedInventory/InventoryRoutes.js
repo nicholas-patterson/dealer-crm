bb718b970c3ce85444c1c6745d094bc04f591786
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-15 19:20:49 -0400

    :sparkles: adds emitter and used inv notif and delete inv notification need to do edit

--- backend/UsedInventory/InventoryRoutes.js
+++ backend/UsedInventory/InventoryRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const db = require("../models");
+const emitter = require("../config/io");
 
 // Get All Used inventory
 router.get("/", async (req, res) => {
@@ -44,6 +45,10 @@ router.post("/add", async (req, res) => {
     console.log(newInventory);
     res.status(201).json(newInventory);
     // Notification that dealer added an item to Used Inventory
+    emitter.emitToDealer(req.session.dealer_user.id, "used_inventory_added", {
+      inventory: newInventory,
+      message: "You added an item to used inventory"
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -90,6 +95,14 @@ router.delete("/delete/:id", (req, res) => {
         .then(deletedInventory => {
           console.log("DELETED INVENTORY", deletedInventory);
           if (deletedInventory === 1) {
+            emitter.emitToDealer(
+              inventory.dealer_id,
+              "used_inventory_deleted",
+              {
+                inventory: inventory,
+                message: "You deleted an item from used inventory"
+              }
+            );
             res.status(200).json({ deletedInventory: inventory });
             // Notification that dealer has deleted a used inventory item
           }

c58a33a15d9597e63952133be673399ab2f68786
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-22 21:53:32 -0400

    :sparkles: adds dealer-notifications migration
    adds dealer-notifications migration

---
+++ backend/migrations/20200322203433-create-dealer-notification.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('DealerNotifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      data: {
+        type: Sequelize.JSON
+      },
+      dealer_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('DealerNotifications');
+  }
+};

d9cc0d741e7eeb3e847247ee2eb94aafe6ba5495
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-22 21:52:52 -0400

    :sparkles: adds notifications to db with sequelize create
    adds notifications to db with sequelize create

--- backend/config/io.js
+++ backend/config/io.js
@@ -2,15 +2,30 @@ const emitter = require("socket.io-emitter")({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT
 });
+const db = require("../models");
 
 const emitToDealer = function(id, title, data) {
-  if (id) emitter.in(`dealer:${id}`).emit("message", { title, data });
+  if (id) {
+    emitter.in(`dealer:${id}`).emit("message", { title, data });
+    db.DealerNotification.create({
+      title,
+      data,
+      dealer_id: id
+    });
+  }
 };
 const emitToAllDealer = function(id, title, data) {
   if (id) emitter.in(`dealer`).emit("message", { title, data });
 };
 const emitToSalesman = function(id, title, data) {
-  if (id) emitter.in(`salesman:${id}`).emit("message", { title, data });
+  if (id) {
+    emitter.in(`salesman:${id}`).emit("message", { title, data });
+    db.SalesmanNotification.create({
+      title,
+      data,
+      salesmans_id: id
+    });
+  }
 };
 const emitToAllSalesman = function(id, title, data) {
   if (id) emitter.in(`salesman`).emit("message", { title, data });

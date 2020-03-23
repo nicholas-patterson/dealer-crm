9bacda1bc24e90132feb4f00903cc4fd9871d52f
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-22 22:06:55 -0400

    :sparkles: adds reducer to get dealer leads
    adds reducer to get dealer *notifications

---
+++ src/reducers/getDealerNotificationsReducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+  notifications: [],
+  loading: false,
+  error: ""
+};
+
+export const getDealerNotificationsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_DEALER_NOTIFICATIONS_START":
+      return {
+        ...state,
+        loading: true
+      };
+
+    case "GET_DEALER_NOTIFICATIONS_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        notifications: action.payload
+      };
+
+    case "GET_DEALER_NOTIFICATIONS_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};

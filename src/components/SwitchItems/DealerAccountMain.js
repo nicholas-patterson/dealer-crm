d4be6b2369538248b7b387c6650d0f0c86ac2cd4
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-22 22:01:22 -0400

    :recycle: removing state from mapStateToProps
    removing state from mapStateToProps

--- src/components/SwitchItems/DealerAccountMain.js
+++ src/components/SwitchItems/DealerAccountMain.js
@@ -74,183 +74,185 @@ const DealerAccountMain = props => {
   return (
     <>
       {props.errors ? <Banner title={props.errors} visibleTime={3000} /> : null}
-      <h2 className="pi-title">My Details</h2>
-      <h4 className="pi-subtitle">Personal Information</h4>
+      <div>
+        <h2 className="pi-title">My Details</h2>
+        <h4 className="pi-subtitle">Personal Information</h4>
 
-      <div className="personal--information">
-        <div className="personal--information__desc">
-          <p>
-            Review and update any personal information that you find necessary
-            and then hit save.
-          </p>
-        </div>
-        <div className="personal--information__form-section">
-          <form
-            className="personal--information__fields"
-            onSubmit={handlePersonalInformationSection}
-          >
-            <div className="pi-field">
-              <input
-                type="text"
-                name="dealer_username"
-                id="username"
-                autoComplete="off"
-                required
-                className="input-group"
-                value={personalInformation.dealer_username}
-                onChange={handlePersonalInformationChange}
-              />
-              <label htmlFor="inputField" className="pi-label-name">
-                <span className="pi-content-name">Username</span>
-              </label>
-            </div>
-            <div className="pi-field" id="last_name_grid_column">
-              <input
-                type="text"
-                name="dealer_password"
-                id="pass"
-                autoComplete="off"
-                required
-                className="input-group"
-                value={personalInformation.dealer_password}
-                onChange={handlePersonalInformationChange}
-              />
-              <label htmlFor="inputField" className="pi-label-name">
-                <span className="pi-content-name">Password</span>
-              </label>
-            </div>
+        <div className="personal--information">
+          <div className="personal--information__desc">
+            <p>
+              Review and update any personal information that you find necessary
+              and then hit save.
+            </p>
+          </div>
+          <div className="personal--information__form-section">
+            <form
+              className="personal--information__fields"
+              onSubmit={handlePersonalInformationSection}
+            >
+              <div className="pi-field">
+                <input
+                  type="text"
+                  name="dealer_username"
+                  id="username"
+                  autoComplete="off"
+                  required
+                  className="input-group"
+                  value={personalInformation.dealer_username}
+                  onChange={handlePersonalInformationChange}
+                />
+                <label htmlFor="inputField" className="pi-label-name">
+                  <span className="pi-content-name">Username</span>
+                </label>
+              </div>
+              <div className="pi-field" id="last_name_grid_column">
+                <input
+                  type="text"
+                  name="dealer_password"
+                  id="pass"
+                  autoComplete="off"
+                  required
+                  className="input-group"
+                  value={personalInformation.dealer_password}
+                  onChange={handlePersonalInformationChange}
+                />
+                <label htmlFor="inputField" className="pi-label-name">
+                  <span className="pi-content-name">Password</span>
+                </label>
+              </div>
 
-            <button
-              type="submit"
-              onClick={() => props.updateUsername(personalInformation)}
-              className="ea-submitBtn"
-              className="pi-submitBtn"
+              <button
+                type="submit"
+                onClick={() => props.updateUsername(personalInformation)}
+                className="ea-submitBtn"
+                className="pi-submitBtn"
+              >
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+        <h4 className="ea-subtitle">E-mail Address</h4>
+        <div className="email--address__information">
+          <div className="email--address__desc">
+            <p>
+              Review and update your email adress, Double check accuracy.
+              Password must be used for validation.
+            </p>
+          </div>
+          <div className="email--address__form-section">
+            <form
+              className="email--address__fields"
+              onSubmit={handleEmailSection}
             >
-              Save
-            </button>
-          </form>
+              <div className="ea-field">
+                <input
+                  type="email"
+                  name="dealer_email"
+                  id="email"
+                  className="ea-group"
+                  autoComplete="off"
+                  required
+                  value={emailInformation.dealer_email}
+                  onChange={handleEmailInformation}
+                />
+                <label htmlFor="inputField" className="ea-label-name">
+                  <span className="ea-content-name">E-mail</span>
+                </label>
+              </div>
+              {/* User must type in correct password to update email */}
+              <div className="ea-field">
+                <input
+                  type="password"
+                  name="dealer_password"
+                  id="password"
+                  className="ea-group"
+                  autoComplete="off"
+                  required
+                  value={emailInformation.dealer_password}
+                  onChange={handleEmailInformation}
+                />
+                <label htmlFor="inputField" className="ea-label-name">
+                  <span className="ea-content-name">Password</span>
+                </label>
+              </div>
+              <button
+                type="submit"
+                onClick={() => props.updateEmail(emailInformation)}
+                className="ea-submitBtn"
+              >
+                Save
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-      <h4 className="ea-subtitle">E-mail Address</h4>
-      <div className="email--address__information">
-        <div className="email--address__desc">
-          <p>
-            Review and update your email adress, Double check accuracy. Password
-            must be used for validation.
-          </p>
-        </div>
-        <div className="email--address__form-section">
-          <form
-            className="email--address__fields"
-            onSubmit={handleEmailSection}
-          >
-            <div className="ea-field">
-              <input
-                type="email"
-                name="dealer_email"
-                id="email"
-                className="ea-group"
-                autoComplete="off"
-                required
-                value={emailInformation.dealer_email}
-                onChange={handleEmailInformation}
-              />
-              <label htmlFor="inputField" className="ea-label-name">
-                <span className="ea-content-name">E-mail</span>
-              </label>
-            </div>
-            {/* User must type in correct password to update email */}
-            <div className="ea-field">
-              <input
-                type="password"
-                name="dealer_password"
-                id="password"
-                className="ea-group"
-                autoComplete="off"
-                required
-                value={emailInformation.dealer_password}
-                onChange={handleEmailInformation}
-              />
-              <label htmlFor="inputField" className="ea-label-name">
-                <span className="ea-content-name">Password</span>
-              </label>
-            </div>
-            <button
-              type="submit"
-              onClick={() => props.updateEmail(emailInformation)}
-              className="ea-submitBtn"
+        <h4 className="pw-subtitle">Password</h4>
+        <div className="password--information">
+          <div className="password--information__desc">
+            <p>
+              To change password, enter your current password and then your new
+              password; be sure to confirm your new password.
+            </p>
+          </div>
+          <div className="password--information__form-section">
+            <form
+              onSubmit={handlePasswordSection}
+              className="password__information__fields"
             >
-              Save
-            </button>
-          </form>
-        </div>
-      </div>
-      <h4 className="pw-subtitle">Password</h4>
-      <div className="password--information">
-        <div className="password--information__desc">
-          <p>
-            To change password, enter your current password and then your new
-            password; be sure to confirm your new password.
-          </p>
-        </div>
-        <div className="password--information__form-section">
-          <form
-            onSubmit={handlePasswordSection}
-            className="password__information__fields"
-          >
-            <div className="pw-field">
-              <input
-                type="password"
-                name="current_password"
-                id="current_password"
-                className="pw-group"
-                autoComplete="off"
-                required
-                value={passwordInformation.current_password}
-                onChange={handlePasswordInformation}
-              />
-              <label htmlFor="inputField" className="pw-label-name">
-                <span className="pw-content-name">Current Password</span>
-              </label>
-            </div>
-            <div className="pw-field">
-              <input
-                type="password"
-                name="new_password"
-                id="new_password"
-                className="pw-group"
-                autoComplete="off"
-                required
-                value={passwordInformation.new_password}
-                onChange={handlePasswordInformation}
-              />
-              <label htmlFor="inputField" className="pw-label-name">
-                <span className="pw-content-name">New Password</span>
-              </label>
-            </div>
-            <div className="pw-field">
-              <input
-                type="password"
-                name="confirm_new_password"
-                id="confirm_new_password"
-                className="pw-group"
-                autoComplete="off"
-                required
-                value={passwordInformation.confirm_new_password}
-                onChange={handlePasswordInformation}
-              />
-              <label htmlFor="inputField" className="pw-label-name">
-                <span className="pw-content-name"> Confirm Password</span>
-              </label>
-            </div>
-            <button
-              onClick={() => props.updatePassword(passwordInformation)}
-              type="submit"
-              className="pw-submitBtn"
-            >
-              Save
-            </button>
-          </form>
+              <div className="pw-field">
+                <input
+                  type="password"
+                  name="current_password"
+                  id="current_password"
+                  className="pw-group"
+                  autoComplete="off"
+                  required
+                  value={passwordInformation.current_password}
+                  onChange={handlePasswordInformation}
+                />
+                <label htmlFor="inputField" className="pw-label-name">
+                  <span className="pw-content-name">Current Password</span>
+                </label>
+              </div>
+              <div className="pw-field">
+                <input
+                  type="password"
+                  name="new_password"
+                  id="new_password"
+                  className="pw-group"
+                  autoComplete="off"
+                  required
+                  value={passwordInformation.new_password}
+                  onChange={handlePasswordInformation}
+                />
+                <label htmlFor="inputField" className="pw-label-name">
+                  <span className="pw-content-name">New Password</span>
+                </label>
+              </div>
+              <div className="pw-field">
+                <input
+                  type="password"
+                  name="confirm_new_password"
+                  id="confirm_new_password"
+                  className="pw-group"
+                  autoComplete="off"
+                  required
+                  value={passwordInformation.confirm_new_password}
+                  onChange={handlePasswordInformation}
+                />
+                <label htmlFor="inputField" className="pw-label-name">
+                  <span className="pw-content-name"> Confirm Password</span>
+                </label>
+              </div>
+              <button
+                onClick={() => props.updatePassword(passwordInformation)}
+                type="submit"
+                className="pw-submitBtn"
+              >
+                Save
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
@@ -260,11 +262,8 @@ const DealerAccountMain = props => {
 const mapStateToProps = state => {
   console.log("MSTP", state);
   return {
-    dealer:
-      state.loginReducer.dealer_email || state.loginReducer.user.dealer_email,
-    dealer_username:
-      state.loginReducer.dealer_username ||
-      state.loginReducer.user.dealer_username,
+    dealer: state.loginReducer.user.dealer_email,
+    dealer_username: state.loginReducer.user.dealer_username,
     errors: state.loginReducer.error
   };
 };

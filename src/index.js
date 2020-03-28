dc8bb5e33b5e54b47d8cc983747311e111099f0f
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-27 22:01:54 -0400

    :sparkles: adds loading to PersistGate
    adds loading to PersistGate

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./Persistor";
 import { PersistGate } from "redux-persist/integration/react";
+import Loading from "./components/Loading";
 const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate loading={<Loading />} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>,

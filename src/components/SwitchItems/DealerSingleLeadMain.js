97d43c322dbcec5c39fd267d5634dcd8c46a8c2e
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-03-28 23:10:53 -0400

    :sparkles: adds data-label attrbute to td tag to use content() in scss
    adds data-label attrbute to td tag to use content() in scss

--- src/components/SwitchItems/DealerSingleLeadMain.js
+++ src/components/SwitchItems/DealerSingleLeadMain.js
@@ -190,21 +190,21 @@ const DealerSingleLeadMain = ({
         </Portal>
       ) : null}
       <tr>
-        <td>
+        <td data-label="Full Name">
           {lead.lead_firstname} {lead.lead_lastname}
         </td>
-        <td>
+        <td data-label="Address">
           {lead.lead_street}, {lead.lead_city}, {lead.lead_state}{" "}
         </td>
-        <td>{lead.lead_type}</td>
-        <td className="edit-style">
+        <td data-label="Type">{lead.lead_type}</td>
+        <td data-label="Edit" className="edit-style">
           <Icon
             onClick={handleLeadEdit}
             className="edit-icon"
             icon={editOutline}
           />
         </td>
-        <td className="delete-style">
+        <td data-label="Delete" className="delete-style">
           <Icon
             onClick={() => handleLeadDelete(lead.id)}
             className="del-icon"

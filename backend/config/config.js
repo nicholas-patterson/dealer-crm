dc52a7ce6e80572f3e9d76e9649652b666b76c0c
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-02 01:28:27 -0400

    :sparkles: adds onfig for test database
    adds onfig for test database

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,11 +9,11 @@ module.exports = {
     dialect: "postgres"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   },
   production: {
     username: "root",
